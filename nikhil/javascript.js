192496bdf24540df53f038f0ac226d16d751df10
nikhil-12345-max <jatothnikhil865@gmail.com>
2025-07-23 15:27:25 +0530

    wersg

---
+++ nikhil/javascript.js
@@ -0,0 +1,97 @@
+
+//document.getElementById("mybutton").addEventListener("click",function(){
+// alert("button was clicked!");
+// });
+
+
+//document.getElementById("myElement").addEventListener("mouseover",function(){
+//this.style.backgroundColor="blue";
+//});
+ 
+
+//document.getElementById("myElement").addEventListener("mouseout",function(){
+//this.style.backgroundColor="green";
+//});
+
+//document.addEventListener("keydown",function(event){
+//console.log("You pressed:"+event.key);
+//});
+
+//document.getElementById("keyup").addEventListener("keyup",function(event){
+//console.log("You released:"+event.key);
+//});
+document
+ 
+
+
+//document.getElementById("myInput").addEventListener("input",function(){
+//console.log("Current input:" + this.value);
+//});
+
+//document.getElementById("myInput").addEventListener("change",function(){
+//alert("Input changed to:" + this.value);
+//});
+
+//document.getElementById("myForm").addEventListener("submit",function(event){
+//event.preventDefault();
+//alert("Form submitted!");
+
+//});
+
+//document.getElementById("dblclick").addEventListener("dblclick",function(){
+   // alert("double click detected!");
+//});
+
+//document.getElementById("uu").addEventListener("focus",function(){
+  //  this.style.backgroundColor="lightblue";
+//});
+
+
+//document.getElementById("uu").addEventListener("blur",function(){
+  //  this.style.backgroundColor="red";
+//});
+
+//document.getElementById("ip").addEventListener("input",function(event){
+  //  document.getElementById("para").innerText=event.target.value;
+//});
+
+//document.getElementById("para").addEventListener("dblclick",function(){
+  //  this.style.color = "red"
+//})
+
+//let count=0;
+//document.getElementById("decrement").addEventListener("click",function(){
+    count=count-1;
+
+// document.getElementById("count").innerHTML=count;});
+// document.getElementById("increment").addEventListener("click",function(){
+//     count=count+1;
+//     document.getElementById("count").innerHTML=count
+// });//
+
+
+//let fruits=["orange","apple"]
+fruits.push("grape");
+console.log(fruits)
+
+let days=["monday","tuesday","wednesday"];
+let firstDay=days.shift();
+console.log(days);
+console.log(firstDay)//
+
+
+let numb=[1,2,3,4];
+let add=numbers.map(num=>num+2);
+console.log(add);
+console.log(numbers);//
+
+
+
+const ners=[1,2,3,4]
+const evenNumbers=numbers.filter(num=>num%2==0);
+console.log(evenNumbers)
+
+const numbers = [1, 2, 3, 4];
+const sum = numbers.reduce((acc, num) => acc + num, 0);
+console.log(sum);
+
